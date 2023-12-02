fd6bfc7ab80c136378aa0788f3faf3417c9a4247
Brandon <abrandon1999@yahoo.com>
2023-12-02 13:26:57 -0500

    Initial Comments

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const addNumber = (a, b) => {
+    return a + b;
+};
+const result = addNumber(1, 2); //Error
+console.log(result);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const addNumber = (a: number, b: number) => {
+    return a + b;
+};
+const result = addNumber(1, 2); //Error
+console.log(result);
