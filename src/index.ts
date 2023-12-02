45f65f4805a418b01a16da170aabe5af79915131
Brandon <abrandon1999@yahoo.com>
2023-12-02 14:33:21 -0500

    Install and Configure local/Remote Mongo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+const mongoConnect2 = 'mongodb://localhost/store';
+const mongoConnect = 'mongodb+srv://abrandon1999:PyW6sGEAJZjVQ7pP@cluster0.edquirp.mongodb.net/';
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(mongoConnect)
+    .then(() => {
+        console.log('Connected to MongoDB...');
+        const port = process.env.Port || 3000;
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    })
+    .catch((err) => console.log("Couldn't connect to MongoDB...", err));
 
 app.get('/', (req: Request, res: Response) => {
     return res.send('Hello Node_Store');
 });
-
-const port = process.env.Port || 3000;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
